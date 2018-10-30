af21e6d49b962769f96604ad4d0922460e519d11
Michael Devery <michael@compucorp.co.uk>
2018-10-30 16:33:07 +0000

    scale-generator passing tests

--- scale-generator/scale-generator.go
+++ scale-generator/scale-generator.go
@@ -2,7 +2,14 @@
 package scale
 
 import (
-	"fmt"
+	"strings"
+)
+
+const (
+	chromaticScaleLenght = 12
+	pitchNotationFlats   = 'F'
+	pitchNotationSharps  = 'S'
+	pitchNotationNone    = 'N'
 )
 
 type pitch struct {
@@ -11,48 +18,67 @@ type pitch struct {
 	next          *pitch
 }
 
-func (p pitch) toString(notationType string) string {
+func (p *pitch) toString(tonic string) string {
+	notationType := getPitchNotationType(tonic)
+
 	switch notationType {
-	case "N":
+	case pitchNotationNone:
 		return p.sharpNotation
-	case "S":
+	case pitchNotationSharps:
 		return p.sharpNotation
-	case "F":
+	case pitchNotationFlats:
 		return p.flatNotation
 	}
 
-	panic("Could not handle notation type" + notationType)
+	panic("Could not handle notation type")
+}
+
+func (p *pitch) isEqual(note string) bool {
+	return strings.ToLower(note) == strings.ToLower(p.sharpNotation) ||
+		strings.ToLower(note) == strings.ToLower(p.flatNotation)
 }
 
 type chromaticScale struct {
 	current *pitch
 }
 
-func (s *chromaticScale) getNext() *pitch {
+func (s *chromaticScale) next() {
 	s.current = s.current.next
-
-	return s.current
 }
 
-func (s *chromaticScale) moveTo(note string) {
-	for {
-		if s.current.flatNotation == note || s.current.sharpNotation == note {
-			return
-		}
-		s.getNext()
+func (s *chromaticScale) skip(num int) {
+	for num > 0 {
+		s.next()
+		num--
 	}
 }
 
-func (s *chromaticScale) getSequence(tonic string, length int) []string {
-	result := make([]string, length)
+func (s *chromaticScale) moveTo(note string) {
+	for i := 0; i < chromaticScaleLenght; i++ {
+		if s.current.isEqual(note) {
+			return
+		}
+		s.next()
+	}
+
+	panic("Could not find note " + note + " in chromatic scale")
+}
+
+func (s *chromaticScale) getScale(tonic string, interval string) []string {
+	result := make([]string, len(interval))
 	s.moveTo(tonic)
-	notationType := getPitchNotationType(tonic)
 
-	fmt.Println("Notation type: " + notationType)
+	for i := 0; i < len(interval); i++ {
+		result[i] = s.current.toString(tonic)
 
-	for i := 0; i < length; i++ {
-		result[i] = s.current.toString(notationType)
-		s.getNext()
+		switch interval[i] {
+		case 'M':
+			s.skip(2)
+		case 'A':
+			s.skip(3)
+		case 'm':
+			s.next()
+		}
 	}
 
 	return result
@@ -60,31 +86,33 @@ func (s *chromaticScale) getSequence(tonic string, length int) []string {
 
 // Scale returns a musical scale from the provided tonic and interval
 func Scale(tonic string, interval string) []string {
-	scale := createScale()
+	scale := createChromaticScale()
 
-	return scale.getSequence(tonic, 12)
+	if interval == "" {
+		// create basic interval with no skips for chromatic scale
+		interval = strings.Repeat("m", chromaticScaleLenght)
+	}
+
+	return scale.getScale(tonic, interval)
 }
 
-func getPitchNotationType(tonic string) string {
+func getPitchNotationType(tonic string) byte {
 	for _, match := range []string{"C", "a"} {
 		if tonic == match {
-			// none
-			return "N"
+			return pitchNotationNone
 		}
 	}
 
 	for _, match := range []string{"G", "D", "A", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#"} {
 		if tonic == match {
-			// sharps
-			return "S"
+			return pitchNotationSharps
 		}
 	}
 
-	// flats
-	return "F"
+	return pitchNotationFlats
 }
 
-func createScale() *chromaticScale {
+func createChromaticScale() *chromaticScale {
 	ab := pitch{"G#", "Ab", nil}
 	g := pitch{"G", "G", &ab}
 	gb := pitch{"F#", "Gb", &g}
