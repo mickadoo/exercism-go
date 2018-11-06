80f06e5f8dd9d35b4766ccf5b14c9a9725182eec
Michael Devery <michael@compucorp.co.uk>
2018-11-06 15:30:49 +0000

    remove uneccessary variable thanks to mentor feedback

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -8,24 +8,20 @@ import (
 // Convert takes a number and converts it to a modified string based on its factors
 func Convert(num int) string {
 	result := ""
-	hasFactor := false
 
 	if num%3 == 0 {
 		result = "Pling"
-		hasFactor = true
 	}
 
 	if num%5 == 0 {
 		result += "Plang"
-		hasFactor = true
 	}
 
 	if num%7 == 0 {
 		result += "Plong"
-		hasFactor = true
 	}
 
-	if hasFactor {
+	if result != "" {
 		return result
 	}
 
