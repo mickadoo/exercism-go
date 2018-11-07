8556186f86d5cb5cb7c36e89733a7a93fd1bee37
Michael Devery <michaeldevery@gmail.com>
2018-11-07 21:56:29 +0000

    add etl

---
+++ etl/etl.go
@@ -0,0 +1,19 @@
+//Package etl is for extracting, transforming and loading - converting legacy data to a new format
+package etl
+
+import (
+	"strings"
+)
+
+// Transform takes old scoring data and transforms it to the new format
+func Transform(oldSystem map[int][]string) map[string]int {
+	newSystem := make(map[string]int, 26)
+
+	for score, letters := range oldSystem {
+		for _, letter := range letters {
+			newSystem[strings.ToLower(letter)] = score
+		}
+	}
+
+	return newSystem
+}
