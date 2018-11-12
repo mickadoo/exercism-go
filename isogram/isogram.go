716fac76f1dc714a8b96d74e98d4009f65fea31f
Michael Devery <michael@compucorp.co.uk>
2018-11-12 14:59:30 +0000

    use bitwise ops instead of map

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -3,21 +3,28 @@ package isogram
 
 import (
 	"strings"
-	"unicode"
 )
 
-// IsIsogram checks if a word is an isogram, i.e. it has no repeating letters
+// IsIsogram checks if a word is an isogram, i.e. it has no repeating letters. It only works for words with ascii characters
 func IsIsogram(word string) bool {
-	charCounts := make(map[rune]int)
 	word = strings.ToLower(word)
+	var charactersFound int32
 
 	for _, letter := range word {
-		if unicode.IsLetter(letter) {
-			charCounts[letter]++
+		// get the numeric representation (0-25) by subtracting the lowest alphabetic character
+		flag := uint(letter - 'a')
+
+		// ignore characters outside of a-z
+		if flag > 25 {
+			continue
 		}
-		if charCounts[letter] > 1 {
+
+		// check if this flag was already set
+		if charactersFound&(1<<flag) > 0 {
 			return false
 		}
+		// set the bit for this flag to 1
+		charactersFound ^= 1 << flag
 	}
 
 	return true
