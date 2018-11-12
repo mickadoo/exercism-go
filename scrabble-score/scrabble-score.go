3ae732eab954a930fd6e5a5ee828dab2c726c718
Michael Devery <michael@compucorp.co.uk>
2018-11-12 14:17:10 +0000

    use switch statement on mentor advice as it's faster

--- scrabble-score/scrabble-score.go
+++ scrabble-score/scrabble-score.go
@@ -5,43 +5,73 @@ import (
 	"strings"
 )
 
-var scoresMap = map[rune]int{
-	'A': 1,
-	'B': 3,
-	'C': 3,
-	'D': 2,
-	'E': 1,
-	'F': 4,
-	'G': 2,
-	'H': 4,
-	'I': 1,
-	'J': 8,
-	'K': 5,
-	'L': 1,
-	'M': 3,
-	'N': 1,
-	'O': 1,
-	'P': 3,
-	'Q': 10,
-	'R': 1,
-	'S': 1,
-	'T': 1,
-	'U': 1,
-	'V': 4,
-	'W': 4,
-	'X': 8,
-	'Y': 4,
-	'Z': 10,
-}
-
 // Score computes the scrabble score for a word
 func Score(word string) int {
 	score := 0
 	word = strings.ToUpper(word)
 
-	for _, i := range word {
-		score += scoresMap[i]
+	for _, letter := range word {
+		score += getLetterScore(letter)
 	}
 
 	return score
+}
+
+func getLetterScore(letter rune) int {
+	switch letter {
+	case 'A':
+		return 1
+	case 'B':
+		return 3
+	case 'C':
+		return 3
+	case 'D':
+		return 2
+	case 'E':
+		return 1
+	case 'F':
+		return 4
+	case 'G':
+		return 2
+	case 'H':
+		return 4
+	case 'I':
+		return 1
+	case 'J':
+		return 8
+	case 'K':
+		return 5
+	case 'L':
+		return 1
+	case 'M':
+		return 3
+	case 'N':
+		return 1
+	case 'O':
+		return 1
+	case 'P':
+		return 3
+	case 'Q':
+		return 10
+	case 'R':
+		return 1
+	case 'S':
+		return 1
+	case 'T':
+		return 1
+	case 'U':
+		return 1
+	case 'V':
+		return 4
+	case 'W':
+		return 4
+	case 'X':
+		return 8
+	case 'Y':
+		return 4
+	case 'Z':
+		return 10
+	}
+
+	panic("Unknown letter")
 }
