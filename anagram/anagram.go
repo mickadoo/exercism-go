a4c8613da30d780abc36962aa96ed07843b5e74b
Michael Devery <michael@compucorp.co.uk>
2018-11-15 11:35:24 +0000

    add anagram

---
+++ anagram/anagram.go
@@ -0,0 +1,36 @@
+// Package anagram is used for detecting anagrams
+package anagram
+
+import "strings"
+
+// Detect checks for anagrams of a given word in a list of candidates
+func Detect(subject string, candidates []string) (result []string) {
+	for _, candidate := range candidates {
+		if isAnagram(subject, candidate) {
+			result = append(result, candidate)
+		}
+	}
+	return result
+}
+
+func isAnagram(original string, anagram string) bool {
+	// make them both the same case
+	original = strings.ToLower(original)
+	anagram = strings.ToLower(anagram)
+
+	// words cannot be anagrams of themselves
+	if original == anagram {
+		return false
+	}
+
+	for _, char := range original {
+		index := strings.IndexRune(anagram, char)
+		if index == -1 {
+			return false
+		}
+		// remove matching character as it can only match once
+		anagram = anagram[0:index] + anagram[index+1:]
+	}
+
+	return anagram == ""
+}
