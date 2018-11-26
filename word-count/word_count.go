9f71dda4889d7d60793be8db0f89db153a2e23f7
Michael Devery <michaeldevery@gmail.com>
2018-11-26 20:26:28 +0000

    improvements

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,8 +1,8 @@
 package wordcount
 
 import (
-	"sort"
 	"strings"
+	"unicode"
 )
 
 // Frequency is a map of words to their frequency counts
@@ -11,41 +11,23 @@ type Frequency map[string]int
 // WordCount counts the number of words in the given input
 func WordCount(input string) Frequency {
 	input = strings.ToLower(input)
-	input = strip(input, []string{"!", "&", "@", "$", "%", "^", ":", "."})
-	input = replace(input, []string{"\n", ",", " '", "' "}, " ")
-	words := strings.Split(input, " ")
+
+	// remove quotations
+	input = strings.Replace(input, " '", " ", -1)
+	input = strings.Replace(input, "' ", " ", -1)
+
+	words := strings.FieldsFunc(input, func(r rune) bool {
+		// don't split on apostrophe
+		if r == '\'' {
+			return false
+		}
+		return unicode.IsSpace(r) || unicode.IsSymbol(r) || unicode.IsPunct(r)
+	})
+
 	result := make(Frequency)
 	for _, word := range words {
-		if word == "" {
-			continue
-		}
 		result[word]++
 	}
 
-	return sortResult(result)
-}
-
-func strip(input string, chars []string) string {
-	return replace(input, chars, "")
-}
-
-func replace(input string, chars []string, replacement string) string {
-	for _, char := range chars {
-		input = strings.Replace(input, char, replacement, -1)
-	}
-	return input
-}
-
-func sortResult(result Frequency) Frequency {
-	keys := make([]string, 0, len(result))
-	for key := range result {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys) //sort by key
-	sorted := make(Frequency)
-	for _, key := range keys {
-		sorted[key] = result[key]
-	}
-
-	return sorted
+	return result
 }
