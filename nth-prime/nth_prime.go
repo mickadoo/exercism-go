060603c8969f94640cd1e2806b9c8c3410301000
Michael Devery <michael@compucorp.co.uk>
2018-11-28 16:19:12 +0000

    use goroutine

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -6,26 +6,51 @@ func Nth(n int) (int, bool) {
 		return 0, false
 	}
 
-	current := 0
-	for i := 2; true; i++ {
-		if isPrime(i) {
-			current++
-			if current == n {
-				return i, true
-			}
+	current := 1
+	c := make(chan int)
+	go getPrimes(c)
+
+	for prime := range c {
+		if current == n {
+			return prime, true
 		}
+		current++
 	}
 
 	// will never reach here
 	return 0, false
 }
 
+func getPrimes(c chan int) {
+	limit := 1000000
+	for i := 0; i < limit; i++ {
+		func(i int) {
+			if isPrime(i) {
+				c <- i
+			}
+		}(i)
+	}
+}
+
 func isPrime(n int) bool {
-	for i := 2; i <= n/2; i++ {
-		if n%i == 0 {
+	// Corner cases
+	if n <= 1 {
+		return false
+	}
+	if n <= 3 {
+		return true
+	}
+
+	// This is checked so that we can skip
+	// middle five numbers in below loop
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+
+	for i := 5; i*i <= n; i = i + 6 {
+		if n%i == 0 || n%(i+2) == 0 {
 			return false
 		}
 	}
-
 	return true
 }
