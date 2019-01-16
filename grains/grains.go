34691c3e9f5a35fbc4465934ea04db3c6f299655
Michael Devery <michaeldevery@gmail.com>
2019-01-16 12:30:22 +0000

    improve perf

--- grains/grains.go
+++ grains/grains.go
@@ -2,7 +2,6 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 // Square returns the number of grains on a certain square
@@ -11,16 +10,10 @@ func Square(square int) (count uint64, err error) {
 		return count, errors.New("square must be between 1 and 64")
 	}
 
-	return uint64(math.Pow(2, float64(square-1))), nil
+	return 1 << uint(square-1), nil
 }
 
 // Total returns the total of all grains of rice on the board
-func Total() uint64 {
-	var expected uint64
-	for i := 1; i < 65; i++ {
-		current, _ := Square(i)
-		expected += current
-	}
-
-	return expected
+func Total() (res uint64) {
+	return res - 1
 }
