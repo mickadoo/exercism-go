3c8de4fdc8376164f1a3eab1d46b17b104a3283e
Michael Devery <michaeldevery@gmail.com>
2019-01-20 22:07:55 +0000

    add some perf improvements from community solutions

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,11 +1,16 @@
 package sieve
 
-// 5651 ns/op
+// 4319 ns/op
 
 // Sieve of Eratosthenes, a method for finding primes up to a limit not using div, /, mod or %
 func Sieve(limit int) (primes []int) {
 	nonPrime := make([]bool, limit+1)
-	for i := 2; i <= limit; i++ {
+	if limit > 1 {
+		// append 2 so we can avoid checking multiples of it below
+		primes = append(primes, 2)
+	}
+
+	for i := 3; i <= limit; i += 2 {
 		if !nonPrime[i] {
 			primes = append(primes, i)
 			appendSelfAndMultiples(i, limit, &nonPrime)
@@ -16,7 +21,10 @@ func Sieve(limit int) (primes []int) {
 }
 
 func appendSelfAndMultiples(num int, limit int, collection *[]bool) {
-	current := num
+	// we only care about from the next square value,
+	// e.g. for num = 6, no point in adding 6 itself since we're past it, plus anything below 36 will have
+	// been added already on the lower side of the multiple (e.g. 12 will have been added by 2 x 6, 18 by 3 x 6)
+	current := num * num
 	for current <= limit {
 		(*collection)[current] = true
 		current = current + num
