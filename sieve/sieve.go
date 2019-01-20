30ac7535a2b7570bfd40862a5bfd023286982b64
Michael Devery <michaeldevery@gmail.com>
2019-01-20 21:40:42 +0000

    use bool slice instead of lookup of int

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,12 +1,12 @@
 package sieve
 
-// 268539 ns/op
+// 6165 ns/op
 
 // Sieve of Eratosthenes, a method for finding primes up to a limit not using div, /, mod or %
 func Sieve(limit int) (primes []int) {
-	var nonPrime []int
+	nonPrime := make([]bool, limit+1)
 	for i := 2; i <= limit; i++ {
-		if !exists(i, nonPrime) {
+		if !nonPrime[i] {
 			primes = append(primes, i)
 			appendSelfAndMultiples(i, limit, &nonPrime)
 		}
@@ -15,21 +15,11 @@ func Sieve(limit int) (primes []int) {
 	return primes
 }
 
-func exists(num int, collection []int) bool {
-	for _, val := range collection {
-		if val == num {
-			return true
-		}
-	}
-
-	return false
-}
-
-func appendSelfAndMultiples(num int, limit int, collection *[]int) {
+func appendSelfAndMultiples(num int, limit int, collection *[]bool) {
 	current := num
 	i := 1
 	for current <= limit {
-		*collection = append(*collection, current)
+		(*collection)[current] = true
 		current = num * i
 		i++
 	}
