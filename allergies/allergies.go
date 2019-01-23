562cb98dd2510729f4cc3a2cbcf9b68df15a6293
Michael Devery <michaeldevery@gmail.com>
2019-01-23 00:10:01 +0000

    alternate approach, slower but easier to maintain

--- allergies/allergies.go
+++ allergies/allergies.go
@@ -1,18 +1,7 @@
 package allergies
 
-// 528 ns/op
-// 73.6 ns/op
-
-const (
-	eggs = 1 << iota
-	peanuts
-	shellfish
-	strawberries
-	tomatoes
-	chocolate
-	pollen
-	cats
-)
+// 761 ns/op
+// 129 ns/op
 
 var allAllergyNames = []string{"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}
 
@@ -33,24 +22,10 @@ func AllergicTo(score uint, substance string) bool {
 }
 
 func getMaskForAllergy(substance string) uint {
-	switch substance {
-	case "eggs":
-		return eggs
-	case "peanuts":
-		return peanuts
-	case "shellfish":
-		return shellfish
-	case "strawberries":
-		return strawberries
-	case "tomatoes":
-		return tomatoes
-	case "chocolate":
-		return chocolate
-	case "pollen":
-		return pollen
-	case "cats":
-		return cats
-	default:
-		return 0
+	for i, name := range allAllergyNames {
+		if name == substance {
+			return 1 << uint(i)
+		}
 	}
+	return 0
 }
